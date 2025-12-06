333fb652b75e19295986c9cacff6aca4df2cba91
ilanmenirom <ilanmenirom@gmail.com>
2025-12-06 13:50:34 +0200

    fixed environment ; added code for parts A,B,C (need to make sure nothing missing)

---
+++ solution.py
@@ -0,0 +1,235 @@
+"""Stereo matching."""
+import numpy as np
+from scipy.signal import convolve2d
+
+
+class Solution:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def ssd_distance(left_image: np.ndarray,
+                     right_image: np.ndarray,
+                     win_size: int,
+                     dsp_range: int) -> np.ndarray:
+        """Compute the SSDD distances tensor.
+
+        Args:
+            left_image: Left image of shape: HxWx3, and type np.double64.
+            right_image: Right image of shape: HxWx3, and type np.double64.
+            win_size: Window size odd integer.
+            dsp_range: Half of the disparity range. The actual range is
+            -dsp_range, -dsp_range + 1, ..., 0, 1, ..., dsp_range.
+
+        Returns:
+            A tensor of the sum of squared differences for every pixel in a
+            window of size win_size X win_size, for the 2*dsp_range + 1
+            possible disparity values. The tensor shape should be:
+            HxWx(2*dsp_range+1).
+        """
+        num_of_rows, num_of_cols = left_image.shape[0], left_image.shape[1]
+        disparity_values = range(-dsp_range, dsp_range + 1)
+        ssdd_tensor = np.zeros((num_of_rows,
+                                num_of_cols,
+                                len(disparity_values)))
+        """INSERT YOUR CODE HERE"""
+
+        # Calculate padding and pad both images (only for window calculations:
+        pad = int(np.floor(win_size / 2))
+        padded_left = Solution.pad_image(left_image, pad)
+        padded_right = Solution.pad_image(right_image, pad)
+
+        # Scan the left image row by row:
+        for row in range(num_of_rows):
+            for col in range(num_of_cols):
+                # Fix the current left-image window
+                left_win = padded_left[row:row + win_size, col:col + win_size]
+                # Scan +- disparity range windows in right image, if they exist:
+                for val in disparity_values:
+                    running_col = col + val
+                    dsp_idx = val + dsp_range
+                    if 0 <= running_col < num_of_cols:
+                        right_win = padded_right[row:row + win_size, running_col:running_col + win_size]
+                        diff = left_win - right_win
+                        ssd = np.sum(diff * diff)
+                    else:
+                        ssd = np.sum(left_win * left_win)
+
+                    ssdd_tensor[row, col, dsp_idx] = ssd
+
+            print('row:', row, 'of:', num_of_rows)
+
+        # Normalize:
+        ssdd_tensor -= ssdd_tensor.min()
+        ssdd_tensor /= ssdd_tensor.max()
+        ssdd_tensor *= 255.0
+
+        return ssdd_tensor
+
+    @staticmethod
+    def naive_labeling(ssdd_tensor: np.ndarray) -> np.ndarray:
+        """Estimate a naive depth estimation from the SSDD tensor.
+
+        Args:
+            ssdd_tensor: A tensor of the sum of squared differences for every
+            pixel in a window of size win_size X win_size, for the
+            2*dsp_range + 1 possible disparity values.
+
+        Evaluate the labels in a naive approach. Each value in the
+        result tensor should contain the disparity matching minimal ssd (sum of
+        squared difference).
+
+        Returns:
+            Naive labels HxW matrix.
+        """
+        # you can erase the label_no_smooth initialization.
+        label_no_smooth = np.zeros((ssdd_tensor.shape[0], ssdd_tensor.shape[1]))
+        """INSERT YOUR CODE HERE"""
+        label_no_smooth = np.argmin(ssdd_tensor, axis=2)
+        return label_no_smooth
+
+    @staticmethod
+    def dp_grade_slice(c_slice: np.ndarray, p1: float, p2: float) -> np.ndarray:
+        """Calculate the scores matrix for slice c_slice.
+
+        Calculate the scores slice which for each column and disparity value
+        states the score of the best route. The scores slice is of shape:
+        (2*dsp_range + 1)xW.
+
+        Args:
+            c_slice: A slice of the ssdd tensor.
+            p1: penalty for taking disparity value with 1 offset.
+            p2: penalty for taking disparity value more than 2 offset.
+        Returns:
+            Scores slice which for each column and disparity value states the
+            score of the best route.
+        """
+        num_labels, num_of_cols = c_slice.shape[0], c_slice.shape[1]
+        l_slice = np.zeros((num_labels, num_of_cols))
+        """INSERT YOUR CODE HERE"""
+        # Initialize first row with raw values"
+        l_slice[:, 0] = c_slice[:, 0]
+        pad_object = 1e9 * np.ones(shape=(1, num_of_cols))
+        l_padded = np.concatenate([pad_object, c_slice, pad_object], axis=0)
+
+        # Storing items in memory to avoid computations within loop:
+        l_penalized = l_padded + p2
+        penalty_diff = np.array([p1 - p2, -p2, p1 - p2])
+
+        for col in range(1, num_of_cols):
+            l_padded[:, col] -= l_padded[:, col - 1].min()
+            l_penalized[:, col - 1] = l_padded[:, col - 1] + p2
+            mdc = np.zeros(l_padded.shape[0])
+            for label in range(1, num_labels + 1):
+                vec = l_penalized[:, col - 1].copy()
+                vec[label - 1:label + 2] += penalty_diff  # Subtract penalties from penalized array
+                mdc[label] = vec.min()
+
+            l_padded[:, col] += mdc
+
+        l_slice = l_padded[1:-1, :]
+
+        return l_slice
+
+    def dp_labeling(self,
+                    ssdd_tensor: np.ndarray,
+                    p1: float,
+                    p2: float) -> np.ndarray:
+        """Estimate a depth map using Dynamic Programming.
+
+        (1) Call dp_grade_slice on each row slice of the ssdd tensor.
+        (2) Store each slice in a corresponding l tensor (of shape as ssdd).
+        (3) Finally, for each pixel in l (along each row and column), choose
+        the best disparity value. That is the disparity value which
+        corresponds to the lowest l value in that pixel.
+
+        Args:
+            ssdd_tensor: A tensor of the sum of squared differences for every
+            pixel in a window of size win_size X win_size, for the
+            2*dsp_range + 1 possible disparity values.
+            p1: penalty for taking disparity value with 1 offset.
+            p2: penalty for taking disparity value more than 2 offset.
+        Returns:
+            Dynamic Programming depth estimation matrix of shape HxW.
+        """
+        l = np.zeros_like(ssdd_tensor)
+        """INSERT YOUR CODE HERE"""
+
+        num_of_rows = ssdd_tensor.shape[0]
+        for row in range(num_of_rows):
+            hor_slice = ssdd_tensor[row, :, :].transpose()
+            l[row, :, :] = self.dp_grade_slice(hor_slice, p1, p2).transpose()
+            # print('row:', row, 'of:', num_of_rows)
+
+        return self.naive_labeling(l)
+    def dp_labeling_per_direction(self,
+                                  ssdd_tensor: np.ndarray,
+                                  p1: float,
+                                  p2: float) -> dict:
+        """Return a dictionary of directions to a Dynamic Programming
+        etimation of depth.
+
+        For each direction in 1, ..., 8, calculate scores tensors
+        according to dp_grade_slice and the method which allows you to
+        extract slices along each direction.
+
+        You may use helper methods (functions) that you write on your own.
+        We found `np.diagonal` to be very helpful to extract diagonal slices.
+        `np.unravel_index` might be helpful if you're thinking in MATLAB
+        notations: it's the ind2sub equivalent.
+
+        Args:
+            ssdd_tensor: A tensor of the sum of squared differences for
+            every pixel in a window of size win_size X win_size, for the
+            2*dsp_range + 1 possible disparity values.
+            p1: penalty for taking disparity value with 1 offset.
+            p2: penalty for taking disparity value more than 2 offset.
+
+        Returns:
+            Dictionary int->np.ndarray which maps each direction to the
+            corresponding dynamic programming estimation of depth based on
+            that direction.
+        """
+        num_of_directions = 8
+        l = np.zeros_like(ssdd_tensor)
+        direction_to_slice = {}
+        """INSERT YOUR CODE HERE"""
+        return direction_to_slice
+
+    def sgm_labeling(self, ssdd_tensor: np.ndarray, p1: float, p2: float):
+        """Estimate the depth map according to the SGM algorithm.
+
+        For each direction in 1, ..., 8, calculate scores tensors
+        according to dp_grade_slice and the method which allows you to
+        extract slices along each direction.
+
+        You may use helper methods (functions) that you write on your own.
+        We found `np.diagonal` to be very helpful to extract diagonal slices.
+        `np.unravel_index` might be helpful if you're thinking in MATLAB
+        notations: it's the ind2sub equivalent.
+
+        Args:
+            ssdd_tensor: A tensor of the sum of squared differences for
+            every pixel in a window of size win_size X win_size, for the
+            2*dsp_range + 1 possible disparity values.
+            p1: penalty for taking disparity value with 1 offset.
+            p2: penalty for taking disparity value more than 2 offset.
+
+        Returns:
+            Semi-Global Mapping depth estimation matrix of shape HxW.
+        """
+        num_of_directions = 8
+        l = np.zeros_like(ssdd_tensor)
+        """INSERT YOUR CODE HERE"""
+        return self.naive_labeling(l)
+
+    @staticmethod
+    def pad_image(img, pad_add):
+        if len(img.shape) > 2:
+            pad_size = [img.shape[0] + 2 * pad_add, img.shape[1] + 2 * pad_add, img.shape[2]]
+        else:
+            pad_size = [img.shape[0] + 2 * pad_add, img.shape[1] + 2 * pad_add]
+
+        padded_img = np.zeros(shape=pad_size)
+        padded_img[pad_add:(img.shape[0] + pad_add), pad_add:(img.shape[1] + pad_add)] = img
+        return padded_img
